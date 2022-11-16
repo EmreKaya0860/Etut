1a348052f752f23e93733deb20fe7da68763414f
EmreKaya0860 <h.emrekaya2207@gmail.com>
2022-11-16 22:54:06 +0300

    first commit

---
+++ Odev2/Controllers/IletisimController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Odev2.Models;
+
+
+namespace Odev2.Controllers
+{
+    public class IletisimController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(Bilgiler[] bilgi)
+        {
+
+            //ViewBag.Name = collections["_name"];
+            //ViewBag.Email = collections["email"];
+            //ViewBag.Mesaj = collections["mesaj"];
+            //ViewBag.Check = collections["kontrol"][0];
+
+            if (ModelState.IsValid) // Eğer Model Nesnesi (uye) Validasyon Kuralları Geçerli ise
+            {
+                // Kurallara uyulmuşsa uye nesnesini veritabanına kaydet
+            }
+
+            if (!ModelState.IsValid) // Model Durumu Geçersiz ise
+            {
+                // Kullanıcıya hata mesajı göster
+                ModelState.AddModelError("", "Lütfen Zorunlu Alanları Doldurunuz"); // Bu metotla ekrana kendi hata mesajlarımızı gönderebiliriz
+            }
+
+            return View(bilgi);
+
+            
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
